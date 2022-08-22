da20e2eac6f7e55891d53bf548423660b058aeeb
BigBoi100 <97864297+BigBoi100@users.noreply.github.com>
2022-08-22 11:48:52 +1000

    I've created the document.
    Read the how to know how to use this python file. :D

---
+++ Python hacker.py
@@ -0,0 +1,20 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import requests
+import random
+
+digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+def is_code_valid(code):
+  request = requests.get("https://kahoot.it/reserve/session/" + code)
+  return request
+
+while True:
+	code = ""
+	for i in range(6):
+		code += random.choice(digits)
+	result = is_code_valid(code)
+	if result.status_code == 200:
+		with open("games.txt", "a") as file:
+			file.write(code + "\n")
+		print(code)
